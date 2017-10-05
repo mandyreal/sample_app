fdd67d4c5afabb13517edcf9a9ba6096063d59ec
mandyreal <normandy.real@gmail.com>
2017-10-05 23:16:52 +1100

    added email validation and test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -27,4 +27,13 @@ class UserTest < ActiveSupport::TestCase
 	test "email should not be too long" do
 		@user.email = "a" * 244 + "@example.com"
 	end
+
+	test "reject invalid emaila address" do
+		invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    	invalid_addresses.each do |invalid_address|
+      		@user.email = invalid_address
+      		assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+		end
+	end
 end
