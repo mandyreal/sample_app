6c524748a28ea5e11d450e30ea942b9a250beca3
mandyreal <normandy.real@gmail.com>
2017-10-09 21:47:49 +1100

    added test to validate email uniquess

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -28,12 +28,18 @@ class UserTest < ActiveSupport::TestCase
 		@user.email = "a" * 244 + "@example.com"
 	end
 
-	test "reject invalid emaila address" do
+	test "reject invalid email address" do
 		invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     	invalid_addresses.each do |invalid_address|
       		@user.email = invalid_address
       		assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
 		end
+	end
+
+	test "email address should be unique" do
+		duplicate_user = @user.dup
+		@user.save
+		assert_not duplicate_user.valid?
 	end
 end
